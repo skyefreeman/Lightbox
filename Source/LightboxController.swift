7eafc835de16fad90d4782799e7fa9d35128c613
Skye Freeman <skyefreeman@icloud.com>
2017-08-16 21:50:57 -0400

    removed gradient layer

--- Source/LightboxController.swift
+++ Source/LightboxController.swift
@@ -72,10 +72,10 @@ open class LightboxController: UIViewController {
 
   open fileprivate(set) lazy var overlayView: UIView = { [unowned self] in
     let view = UIView(frame: CGRect.zero)
-    let gradient = CAGradientLayer()
-    let colors = [UIColor(hex: "090909").alpha(0), UIColor(hex: "040404")]
-
-    view.addGradientLayer(colors)
+    //    let gradient = CAGradientLayer()
+    //    let colors = [UIColor(hex: "090909").alpha(0), UIColor(hex: "040404")]
+    //
+    //    view.addGradientLayer(colors)
     view.alpha = 0
 
     return view
